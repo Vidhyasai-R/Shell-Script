5e0b8832e81b1862b31412eada9ac5fd22143e0f
vidhyasai <relangividhyasai@gmail.com>
2024-12-30 12:04:54 +0530

    shell-1..sh

---
+++ Shell-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1=Ramesh
+person2=Suresh
+
+echo "$person1 :: Hi, $person2"
+echo "$person2 :: Hello, $person1"
+echo "$person1 :: How are you? $person2"
+echo "$person2 :: I'm fine, what about you? $person1"
+echo "$person1 :: I'm fine, Thank you for asking me"
